053bc863d7716ddeb11954e057375ab4853ea8c8
Flora <141187313+FloritaZhao@users.noreply.github.com>
2025-06-04 11:12:36 -0700

    move from gold to c23

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,108 @@
+const LETTER_POOL = {
+    A: 9, B: 2, C: 2, D: 4, E: 12,
+    F: 2, G: 3, H: 2, I: 9, J: 1,
+    K: 1, L: 4, M: 2, N: 6, O: 8,
+    P: 2, Q: 1, R: 6, S: 4, T: 6,
+    U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1,
+  };
+const HAND_SIZE = 10;
+  
+
+// wave 01
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const allLetters = [];
+
+  for (const [letter, count] of Object.entries(LETTER_POOL)) {
+    for (let i = 0; i< count; i++) {
+      allLetters.push(letter);
+    }
+  }
+
+  const selTen = [];
+  while (selTen.length < HAND_SIZE) {
+    const randomIndex = Math.floor(Math.random() * allLetters.length);
+    const selectedLetter = allLetters.splice(randomIndex, 1)[0];
+    selTen.push(selectedLetter);
+
+  }
+
+  return selTen;
+
 };
 
+
+// wave 02
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  const letterCount = {};
+  for (const letter of lettersInHand) {
+    letterCount[letter] = (letterCount[letter] || 0) +1;
+  }
+
+  for (const char of input.toUpperCase()) {
+    if (!letterCount[char] || letterCount === 0) {
+      return false;
+    }
+    letterCount[char] -= 1;
+  }
+  return true;
 };
+
+
+// wave 03
+
+const SCORE_CHART = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
+  };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let score = 0;
+
+  for (const letter of word.toUpperCase()) {
+    score += SCORE_CHART[letter] || 0;
+  }
+  
+  if (word.length >= 7 && word.length <=10) {
+    score += 8;
+  }
+  return score;
 };
 
+// wave 04
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  
+  const scoreWords = words.map(word => {
+    return {
+      word: word,
+      score: scoreWord(word)
+    };
+  });
+  
+  let maxScore = 0;
+  for (const entry of scoreWords) {
+    if (entry.score > maxScore) {
+      maxScore = entry.score;
+    }
+  }
+
+  const topWords = scoreWords.filter(entry => entry.score === maxScore);
+  let winner = topWords[0];
+
+  for (const entry of topWords) {
+    if (entry.word.length === 10 && winner.word.length !== 10){
+      winner = entry;
+    } else if (
+      entry.word.length < winner.word.length &&
+      winner.word.length !== 10
+    ) {
+      winner = entry;
+    }
+  }
+  return winner;
+
 };
